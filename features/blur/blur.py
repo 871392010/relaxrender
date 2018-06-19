87a77aea95bdbf7289ed52abfd6d12186354442b
chenyang <39646318+871392010@users.noreply.github.com>
2018-06-19 10:47:50 +0800

    2016.6.19

--- features/blur/blur.py
+++ features/blur/blur.py
@@ -2,12 +2,15 @@ import numpy
 import cv2
 from scipy import ndimage
 
+#采用高斯模糊的方法，高斯模糊即对每个3*3的像素点矩阵进行求平均值从而达到模糊但不失真的效果#
+
+
 def blur(image, districts, scale):
 	if(len(image.shape)!=3):
 		print("error")
 		exit(0)
 		
-	new_image = image.copy()
+	new_image = image.copy()        #对图片进行复制再进行修改#
 	
 	for district in districts:
 		new_image = gauss_blur(new_image, district, scale)
@@ -20,11 +23,13 @@ def gauss_blur(image, district, scale):
 		print("error district")
 		exit(1)
 	
+        #这是高斯模糊的一个标准矩阵#
 	gaussian = numpy.array([[1,2,1],
 							[2,4,2],
 							[1,2,1]]) * 1.0/16
 	
 	new_image = image.copy()
+	#对图片上每个像素点和高斯矩阵进行卷积，卷积次数越高越模糊#
 	for r in range(scale):
 		for i in range(3):
 			new_image[district[0]:district[0]+district[2], district[1]:district[1]+district[3], i] = ndimage.convolve(new_image[district[0]:district[0]+district[2], district[1]:district[1]+district[3], i], gaussian)
@@ -49,6 +54,7 @@ def gauss_blur_version2(image, district, scale):
 		print("error district")
 		exit(1)
 	
+        #这里尝试去修改高斯的矩阵观察效果#
 	gaussian = numpy.array([[1,2,1],
 							[2,100,2],
 							[1,2,1]]) * 1.0/112
